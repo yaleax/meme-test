00de5cd1470dec295f384bfd5b59bac9252d64a0
reuixiy <reuixiy@gmail.com>
2019-08-12 19:55:53 +0800

    test: add gulp to build sw.js

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const shell = require("gulp-shell");
+const workbox = require("workbox-build");
+
+gulp.task("clean", function () {
+    return gulp.src("public", { read: false, allowEmpty: true })
+    .pipe(clean());
+});
+
+gulp.task("hugo-build", shell.task(["hugo --gc --minify"]));
+
+gulp.task("generate-service-worker", () => {
+    return workbox.generateSW({
+        cacheId: "thepolyglotdeveloper",
+        globDirectory: "./public",
+        globPatterns: [
+            "**/*.{css,js,eot,ttf,woff,woff2,otf}"
+        ],
+        swDest: "./public/sw.js",
+        modifyUrlPrefix: {
+            "": "/"
+        },
+        importWorkboxFrom: "disabled",
+        importScripts: ['https://cdn.jsdelivr.net/npm/workbox-sw@latest/build/workbox-sw.min.js'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+        ignoreUrlParametersMatching: [/./],
+        offlineGoogleAnalytics: true,
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+        runtimeCaching: [
+            {
+                urlPattern: /(?:\/)$/,
+                handler: "staleWhileRevalidate",
+                options: {
+                    cacheName: "html",
+                    expiration: {
+                        maxAgeSeconds: 60 * 60 * 24 * 7,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "images",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:mp3|wav|m4a)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "audio",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:m4v|mpg|avi)$/,
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "videos",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            }
+        ],
+    });
+});
+
+gulp.task("build", gulp.series("clean", "hugo-build", "generate-service-worker"));
