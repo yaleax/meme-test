5687932bce5333854aa646383073f43c21bcb7c6
reuixiy <reuixiy@gmail.com>
2019-08-15 23:50:13 +0800

    test: sw.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,41 +15,42 @@ gulp.task("hugo-build", shell.task(["hugo --gc --minify"]));
 gulp.task("generate-service-worker", () => {
     return workbox.generateSW({
         cacheId: "hugo-theme-meme",
+        swDest: "./public/sw.js",
+        ignoreUrlParametersMatching: [/./],
+        offlineGoogleAnalytics: true,
+        cleanupOutdatedCaches: true,
         globDirectory: "./public",
         globPatterns: [
-            "**/*.{html,css,js,woff2}"
+            "**/*.{css,js,json,woff2}",
+            "life/*",
+            "tech/*"
         ],
-        swDest: "./public/sw.js",
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
         modifyUrlPrefix: {
             "": "/"
         },
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-        skipWaiting: true,
-        ignoreUrlParametersMatching: [/./],
-        offlineGoogleAnalytics: true,
-        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
         runtimeCaching: [
             {
-                urlPattern: /(?:\/)$/,
+                urlPattern: /\.(?:html)$/,
                 handler: "cacheFirst",
                 options: {
                     cacheName: "html",
                     expiration: {
-                        maxAgeSeconds: 60 * 60 * 24 * 7,
-                    },
-                },
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 7
+                    }
+                }
             },
             {
                 urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
                 handler: "cacheFirst",
                 options: {
-                    cacheName: "images",
+                    cacheName: "image",
                     expiration: {
                         maxEntries: 1000,
-                        maxAgeSeconds: 60 * 60 * 24 * 365,
-                    },
-                },
+                        maxAgeSeconds: 60 * 60 * 24 * 365
+                    }
+                }
             },
             {
                 urlPattern: /\.(?:mp3|wav|mp4|webm|ogg)$/,
@@ -58,20 +59,49 @@ gulp.task("generate-service-worker", () => {
                     cacheName: "media",
                     expiration: {
                         maxEntries: 1000,
-                        maxAgeSeconds: 60 * 60 * 24 * 365,
-                    },
-                },
+                        maxAgeSeconds: 60 * 60 * 24 * 365
+                    }
+                }
             },
             {
-                urlPattern: /\.(?:json)$/,
+                urlPattern: /\.(?:eot|ttf|woff|woff2)$/,
                 handler: "cacheFirst",
                 options: {
-                    cacheName: "json",
+                    cacheName: "font",
                     expiration: {
                         maxEntries: 1000,
-                        maxAgeSeconds: 60 * 60 * 24 * 365,
-                    },
-                },
+                        maxAgeSeconds: 60 * 60 * 24 * 365
+                    }
+                }
+            },
+            {
+                urlPattern: new RegExp('^https:\/\/fonts\.googleapis\.com'),
+                handler: "staleWhileRevalidate",
+                options: {
+                    cacheName: "google-fonts-stylesheets",
+                }
+            },
+            {
+                urlPattern: new RegExp('^https:\/\/fonts\.gstatic\.com'),
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "google-fonts-webfonts",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365
+                    }
+                }
+            },
+            {
+                urlPattern: new RegExp('^https:\/\/cdn\.jsdelivr\.net'),
+                handler: "cacheFirst",
+                options: {
+                    cacheName: "static-libs",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365
+                    }
+                }
             }
         ],
     });
