a07b9aef9e585ad470eb8192e659c177824dc5db
reuixiy <reuixiy@gmail.com>
2019-08-16 19:26:52 +0800

    test: update sw.js

--- static/js/updated-notification.js
+++ static/js/updated-notification.js
@@ -2,14 +2,14 @@ workbox.precaching.addPlugins([new workbox.broadcastUpdate.Plugin('sw.update')])
 
 const updatesChannel = new BroadcastChannel('sw.update');
 updatesChannel.addEventListener('message', async (event) => {
-  const {cacheName, updatedUrl} = event.data.payload;
+    let a = document.querySelector('meta[name=theme-color]')
+    , b = document.createElement('div');
+  a && (a.content = '#000'),
+  b.innerHTML = '<style>.app-refresh{background:#000;height:0;line-height:52px;overflow:hidden;position:fixed;top:0;left:0;right:0;z-index:10001;padding:0 18px;transition:all .3s ease;-webkit-transition:all .3s ease;-moz-transition:all .3s ease;-o-transition:all .3s ease;}.app-refresh-wrap{display:flex;color:#fff;font-size:15px;}.app-refresh-wrap label{flex:1;}.app-refresh-show{height:52px;}</style><div class="app-refresh" id="app-refresh"><div class="app-refresh-wrap" onclick="location.reload()"><label>\u5DF2\u66F4\u65B0\u6700\u65B0\u7248\u672C</label><span>\u70B9\u51FB\u5237\u65B0</span></div></div>',
+  document.body.appendChild(b),
+  setTimeout(function() {
+      document.getElementById('app-refresh').className += ' app-refresh-show'
+  }, 16)
 
-  // Do something with cacheName and updatedUrl.
-  // For example, get the cached content and update
-  // the content on the page.
-  const cache = await caches.open(cacheName);
-  const updatedResponse = await cache.match(updatedUrl);
-  const updatedText = await updatedResponse.text();
-
-  console.log(event.data)
+  console.log("log")
 });
