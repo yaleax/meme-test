032d44368ebdc0b22edb95c23dec5a4f84bf2ae1
reuixiy <reuixiy@gmail.com>
2019-08-16 19:22:57 +0800

    test: update sw.js

--- static/js/updated-notification.js
+++ static/js/updated-notification.js
@@ -1,1 +1,15 @@
 workbox.precaching.addPlugins([new workbox.broadcastUpdate.Plugin('sw.update')]);
+
+const updatesChannel = new BroadcastChannel('sw.update');
+updatesChannel.addEventListener('message', async (event) => {
+  const {cacheName, updatedUrl} = event.data.payload;
+
+  // Do something with cacheName and updatedUrl.
+  // For example, get the cached content and update
+  // the content on the page.
+  const cache = await caches.open(cacheName);
+  const updatedResponse = await cache.match(updatedUrl);
+  const updatedText = await updatedResponse.text();
+
+  console.log(event.data)
+});
