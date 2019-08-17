1ab0bfd1343bd5b2f48b99894f14939a833a9a15
reuixiy <reuixiy@gmail.com>
2019-08-17 09:54:49 +0800

    test: update sw.js

--- static/offline.js
+++ static/offline.js
@@ -1,6 +1,9 @@
+workbox.routing.setDefaultHandler(
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
 workbox.routing.setCatchHandler(context => {
     if (context.event.request.destination === 'document') {
         return caches.match('/offline/');
     }
-    return Response.error();
 });
